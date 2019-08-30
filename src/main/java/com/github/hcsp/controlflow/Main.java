205dbc9ad2cc0bb8f97494e8b1533adb022ff794
keliangyong <337215966@qq.com>
2019-08-30 14:46:31 +0800

    done (#57)
    done (#57)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,20 @@ public class Main {
      * @param year 给定的年份数字
      * @return 如果该年份是闰年，返回true，否则返回false
      */
-    public static boolean isLeapYear(int year) {}
+    public static boolean isLeapYear(int year) {
+        boolean isLeapYear = false;
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                isLeapYear = true;
+            }
+
+        } else if (year % 4 == 0) {
+            isLeapYear = true;
+        }
+
+        return isLeapYear;
+    }
 
     public static void main(String[] args) {
         System.out.println(isLeapYear(1999));
