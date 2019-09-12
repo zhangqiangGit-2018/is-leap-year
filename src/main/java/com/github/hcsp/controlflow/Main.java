b0c8b1c5590cf4443132f21686f7293dac521f5c
steven.wan <951520698@qq.com>
2019-09-12 15:03:05 +0800

    判断闰年 (#62)
    判断闰年 (#62)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,17 @@ public class Main {
      * @param year 给定的年份数字
      * @return 如果该年份是闰年，返回true，否则返回false
      */
-    public static boolean isLeapYear(int year) {}
+    public static boolean isLeapYear(int year) {
+        if (year % 100 == 0   ){
+            if (year % 400 == 0){
+                return true;
+            }
+            return false;
+        } else if (year % 4 == 0){
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         System.out.println(isLeapYear(1999));
