1ed0a947c3e9639e7353dfa99fb9568c38731ca1
Storm4542 <417002107@qq.com>
2019-12-03 15:15:21 +0800

    判断闰年

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -11,7 +11,13 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        return year % 4 == 0 || (year % 100 == 0 && year % 400 == 0);
+        if (year % 100 == 0) {
+            return year % 400 == 0;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
