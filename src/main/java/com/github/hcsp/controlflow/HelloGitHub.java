9bd91ff92e7f107c36dd83b2692c0aa39c7b7fce
zhangqiang <15910676714@163.com>
2020-04-12 21:25:49 +0800

    第一次提交代码，文本编辑

---
+++ src/main/java/com/github/hcsp/controlflow/HelloGitHub.java
@@ -0,0 +1,7 @@
+public class StringDemo{
+	public static void mian(String[]args){
+		char[] helloGitHub={'h','e','l','l','o','g','i','t','h','u','b'};
+		String helloString=new String(helloGitHub);
+		System.out.println(helloString);
+	}
+}
